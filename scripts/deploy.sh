9bb12eadd0a04db257aee080ffe8578b4bada7f6
kimyoowan <ywan311@naver.com>
2020-02-18 17:01:54 +0900

    deploy.sh 수정 실행권한에 관리가 권한 부여

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,7 +27,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep *jar | tail -n 1)
 echo "> JAR_Name: $JAR_NAME"
 
 echo ">$JAR_NAME 실행권한 추가"
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
